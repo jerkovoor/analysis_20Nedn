4abfaeb04223bee04aaec79a694bf0fb89242e42
jerkovoor <jeromemkovoor@gmail.com>
2021-06-01 17:17:35 -0500

    Updating

--- analysis_20NednRun.cpp
+++ analysis_20NednRun.cpp
@@ -29,7 +29,7 @@ TChain* MakeChain() {
 
     // chain->Add(InPath+"bkgd_run_6_DD.root");
     // chain->Add(InPath+"bkgd_run_7_DD.root");
-    chain->Add(InPath+"run50_DD.root");
+    chain->Add(InPath+"run16_DD.root");
 
     // if(extTrigger){
     //     for(int run_num=28;run_num<39;run_num++){
