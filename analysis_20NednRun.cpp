e6e14219d430712cac31941d7ab1e6eab94863a6
jerkovoor <jeromemkovoor@gmail.com>
2021-06-14 14:53:40 -0400

    Up

--- analysis_20NednRun.cpp
+++ analysis_20NednRun.cpp
@@ -15,8 +15,10 @@ int main() {
 TChain* MakeChain() {
     auto *chain = new TChain("PixTree");
 
-    bool extTrigger = 0;
-    bool thinTarget = 1;
+    // bool extTrigger = 0;
+    bool thinTarget = 0;
+    bool background = 1;
+    bool collimator = 0;
 
     bool server = 1;//Turn this on to run on the server
 
@@ -33,37 +35,51 @@ TChain* MakeChain() {
     // chain->Add(InPath+"run16_DD.root");
     // chain->Add(InPath+"run50_DD.root");
     //chain->Add(InPath+"calib_60co_3_DD.root");//Hagrid 60Co calibration
-    
-    if(thinTarget){
-    	for(int run_num=19;run_num<54;run_num++){
-             chain->Add(InPath+Form("run%d_DD.root", run_num));
-         }    
-    }else{
-    	for(int run_num=14;run_num<19;run_num++){
-             chain->Add(InPath+Form("run%d_DD.root", run_num));
-         }
-         chain->Add(InPath+"run54_DD.root");
-         chain->Add(InPath+"run55_DD.root");
+
+    if(background){
+       for(int run_num=1;run_num<14;run_num++){
+            chain->Add(InPath+Form("bkgd_run_%d_DD.root", run_num));
+        }
+    }else if(collimator){
+        chain->Add(InPath+"run49_DD.root");
+    }else{//////////////Data//////////////
+        if(thinTarget){//////////////////Thin target//////////////////////
+            for(int run_num=19;run_num<49;run_num++){
+                chain->Add(InPath+Form("run%d_DD.root", run_num));
+            }
+            chain->Add(InPath+"run50_DD.root");
+            chain->Add(InPath+"run51_DD.root");
+            chain->Add(InPath+"run52_DD.root");
+            chain->Add(InPath+"run53_DD.root");
+        }else{//////////////////Thick target///////////////////
+            for(int run_num=14;run_num<19;run_num++){
+                chain->Add(InPath+Form("run%d_DD.root", run_num));
+            }
+            chain->Add(InPath+"run54_DD.root");
+            chain->Add(InPath+"run55_DD.root");
+        }
     }
+    
+    
          
 
-     //if(extTrigger){
-     //    for(int run_num=23;run_num<27;run_num++){
-     //        chain->Add(InPath+Form("run%d_DD.root", run_num));
-     //    }
-     //    for(int run_num=28;run_num<48;run_num++){
-     //        chain->Add(InPath+Form("run%d_DD.root", run_num));
-     //    }
-        
-     //}else{
-     //    for(int run_num=1;run_num<23;run_num++){
-     //        chain->Add(InPath+Form("run%d_DD.root", run_num));
-     //    }
-     //    chain->Add(InPath+"run27_DD.root");
-     //    for(int run_num=48;run_num<56;run_num++){
-     //        chain->Add(InPath+Form("run%d_DD.root", run_num));
-     //    }
-     //}
+    // if(extTrigger){
+    //     for(int run_num=23;run_num<27;run_num++){
+    //         chain->Add(InPath+Form("run%d_DD.root", run_num));
+    //     }
+    //     for(int run_num=28;run_num<48;run_num++){
+    //         chain->Add(InPath+Form("run%d_DD.root", run_num));
+    //     }
+    
+    // }else{
+    //     for(int run_num=1;run_num<23;run_num++){
+    //         chain->Add(InPath+Form("run%d_DD.root", run_num));
+    //     }
+    //     chain->Add(InPath+"run27_DD.root");
+    //     for(int run_num=48;run_num<56;run_num++){
+    //         chain->Add(InPath+Form("run%d_DD.root", run_num));
+    //     }
+    // }
     
 
   
